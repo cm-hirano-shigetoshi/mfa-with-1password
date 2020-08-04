608296b03735f6cc9fb52544f8222965262ed138
Shigetoshi Hirano <hirano.shigetoshi@classmethod.jp>
2020-08-04 23:20:10 +0900

    非同期処理を排除

--- with-password.sh
+++ with-password.sh
@@ -1,21 +1,18 @@
-#!/bin/bash
-set -u
+#!/usr/bin/env bash
+set -eu
 
 readonly ACCOUNT="${ONEPASSWORD_ACCOUNT-YourAccount}"
 readonly AUTH_FILE="$1"
 readonly SERVICE_NAME="$2"
-readonly PANE_ID="${3-}"
-readonly TOOLDIR="$(dirname $0)"
+readonly PANE_ID="$3"
 
+op signin ${ACCOUNT} --output=raw > ${AUTH_FILE}
 pass=$(eval OP_SESSION_${ACCOUNT}=$(cat ${AUTH_FILE}) op get totp ${SERVICE_NAME})
 
-if [[ -z "${pass}" ]]; then
-  tmux split-window "bash ${TOOLDIR}/with-password.sh '${AUTH_FILE}' '${SERVICE_NAME}' '${PANE_ID}'"
-else
+if [[ -n ${pass} ]]; then
   if [[ -n "${PANE_ID}" ]]; then
     tmux send-keys -t ${PANE_ID} "${pass}" C-m
   else
     echo "${pass}"
   fi
 fi
-
