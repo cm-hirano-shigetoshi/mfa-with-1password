efaf9f58e50e16879d2363d562a4e8510b65f068
Shigetoshi Hirano <hirano.shigetoshi@classmethod.jp>
2020-08-04 23:34:19 +0900

    微修正

--- aws-mfa.sh
+++ aws-mfa.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -u
 
 readonly ACCOUNT="${ONEPASSWORD_ACCOUNT-YourAccount}"
@@ -9,13 +9,13 @@ readonly TOOLDIR="$(dirname $0)"
 
 pass=$(eval OP_SESSION_${ACCOUNT}=$(cat ${AUTH_FILE}) op get totp ${SERVICE_NAME})
 
-if [[ -z "${pass}" ]]; then
-  tmux split-window "bash ${TOOLDIR}/with-password.sh '${AUTH_FILE}' '${SERVICE_NAME}' '${PANE_ID}'"
-else
+if [[ -n "${pass}" ]]; then
   if [[ -n "${PANE_ID}" ]]; then
     tmux send-keys -t ${PANE_ID} "${pass}" C-m
   else
     echo "${pass}"
   fi
+else
+  tmux split-window "bash ${TOOLDIR}/with-password.sh '${AUTH_FILE}' '${SERVICE_NAME}' '${PANE_ID}'"
 fi
 
--- with-password.sh
+++ with-password.sh
@@ -9,7 +9,7 @@ readonly PANE_ID="$3"
 op signin ${ACCOUNT} --output=raw > ${AUTH_FILE}
 pass=$(eval OP_SESSION_${ACCOUNT}=$(cat ${AUTH_FILE}) op get totp ${SERVICE_NAME})
 
-if [[ -n ${pass} ]]; then
+if [[ -n "${pass}" ]]; then
   if [[ -n "${PANE_ID}" ]]; then
     tmux send-keys -t ${PANE_ID} "${pass}" C-m
   else
